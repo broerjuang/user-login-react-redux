b8d50bacb0351cbf78b8237506f2a330deb7df4e
broerjuang <juangwiantoro@gmail.com>
2016-11-26 16:24:53 +0700

    landing page React-Route

---
+++ src/components/SignupPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class SignupPage extends React.Component {
+  render() {
+    return (
+      <div className="jumbotron">
+        <h1>Hello From Signup Page!</h1>
+      </div>
+    );
+  }
+}
+
+export default SignupPage;
