7bccdc5890c9a580a2aeb7fbc6c68ba36d915bfb
broerjuang <juangwiantoro@gmail.com>
2016-11-29 11:56:13 +0700

    passport serialize and deserialize

--- server/app.js
+++ server/app.js
@@ -5,12 +5,33 @@ var cors = require('cors');
 var passport = require('passport');
 var cookieParser = require('cookie-parser');
 var LocalStrategy = require('passport-local').Strategy; // Using Capital for class, Strategy is class coy
+var session = require('express-session')
+
+var modelUser = require('./models/user.model'); // we need user model for connecting with passport
 
 var port = 3001;
 var users = require('./routes/users');
 var app = express();
 
 app.use(cors())
+// this should be called after user model is embeded
+// to make user cookies on the browser
+app.use(session({
+  secret: 'bebas',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(modelUser.authenticate()))
+
+// please read more about serialize and deserilize user. For short, it just putting user scheama into separate bite in server
+passport.serializeUser(modelUser.serializeUser());
+passpor.deserializeUser(modelUser.deserializeUser());
+
 
 // require mongoose
 var mongoose = require('mongoose');
